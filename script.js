6d6134089a2e72cbf3aeb506d2db20c0edbddb78
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-04-29 16:47:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+// get all the bold elements
+const boldElements = document.querySelectorAll('strong');
+
 function highlight() {
-    //Write your code here
-
-
+  // loop through each bold element and change its color to green
+  for(let i=0; i<boldElements.length; i++) {
+    boldElements[i].style.color = 'green';
+  }
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+  // loop through each bold element and change its color back to black
+  for(let i=0; i<boldElements.length; i++) {
+    boldElements[i].style.color = 'black';
+  }
 }
